3e72f9ca4bb6d293888b4a354b7a66c5b7cfaad5
nicholassouto <nssouto@hotmail.com>
2023-10-05 15:33:49 -0300

    project finish

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  @Input()
+  gameCover: string = '';
+  @Input()
+  gameLabel: string = '';
+  @Input()
+  gameType: string = 'XPTO | PS4';
+  @Input()
+  gamePrice: string = 'R$ 399,90';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
